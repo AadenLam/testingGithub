5ca85cfd3f923746b48cf85da884367c43b65823
featuredspace39 <113187498+featuredspace39@users.noreply.github.com>
2024-02-21 13:19:16 -0500

    Update Aaden_Lam.java

--- First GitHub Project/src/Aaden_Lam.java
+++ First GitHub Project/src/Aaden_Lam.java
@@ -1,5 +1,7 @@
 public class Aaden_Lam {
     public static void main(String[] args) {
         System.out.println("Hello " + "Aaden Lam");
+
+        System.out.println("Hello other editor!");
     }
 }
